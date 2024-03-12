ccb6e27bcf578655f43250a428e9c983180edbf8
Peter Danenberg <pcd@roxygen.org>
2024-03-12 22:56:56 +0000

    Update for new Gemma format.

--- call.sh
+++ call.sh
@@ -4,7 +4,7 @@ function main() {
     curl -i localhost:8080 \
          -X POST \
          -H 'Content-Type: application/x-www-form-urlencoded' \
-         --data-urlencode 'SpeechResult=Can you explain what recursion is?'
+         --data-urlencode 'SpeechResult=I have a fever'
 }
 
 # Fail early.
--- main.py
+++ main.py
@@ -1,105 +1,118 @@
-import functions_framework
+from http import HTTPStatus
 import json
 import logging
 import re
 
-from twilio.twiml.voice_response import VoiceResponse, Gather, Say
-from http import HTTPStatus
+import functions_framework
 from google.cloud import aiplatform
+from google.cloud import aiplatform_v1
+from twilio.twiml.voice_response import Gather, Say, VoiceResponse
 
 
 def get_param(key: str) -> str:
-    with open("params.json", "r") as file:
-        return json.load(file)[key]
+  with open("params.json", "r") as file:
+    return json.load(file)[key]
 
 
 def say(text):
-    return Say(text, voice="Google.en-US-Neural2-H")
+  return Say(text, voice="Google.en-US-Neural2-H")
 
 
 def generate(prompt):
-    PROMPT = """<start_of_turn>user
-You're Gemma, a helpful agent on the phone; please keep responses short and relevant!<end_of_turn>
+  PROMPT = """<start_of_turn>user
+You're a nurse whose job is to triage patients into one of the specialties: Internal Medicine, Pediatrics, Orthopedics, Cardiology, Neurology. Answer with the specialty only.<end_of_turn>
 <start_of_turn>model
-Ok!<end_of_turn>
+ok</end_of_turn>
 <start_of_turn>user
-{prompt}<end_of_turn>"""
+My tummy hurts<end_of_turn>
+<start_of_turn>model
+Internal Medicine<end_of_turn>
+<start_of_turn>user
+{prompt}<end_of_turn>
+<start_of_turn>model"""
 
-    # Initialize the Vertex AI client
-    client_options = {
-        "api_endpoint": f"{get_param('location')}-aiplatform.googleapis.com"
-    }
-    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+  # Initialize the Vertex AI client
+  client_options = {
+      "api_endpoint": f"{get_param('location')}-aiplatform.googleapis.com"
+  }
+  client = aiplatform_v1.PredictionServiceClient(client_options=client_options)
 
-    # Construct the full path of the endpoint
-    endpoint = client.endpoint_path(
-        project=get_param("project"),
-        location=get_param("location"),
-        endpoint=get_param("endpoint"),
-    )
+  # Construct the full path of the endpoint
+  endpoint = client.endpoint_path(
+      project=get_param("project"),
+      location=get_param("location"),
+      endpoint=get_param("endpoint"),
+  )
 
-    # Prepare your input for the model
-    instances = [
-        {
-            "inputs": PROMPT.format(prompt=prompt),
-            "parameters": {
-                "max_new_tokens": 50,
-                "temperature": 1.0,
-                "top_p": 1.0,
-                "top_k": 1.0,
-            },
-        },
-    ]
-    # Make the prediction request
-    response = client.predict(endpoint=endpoint, instances=instances)
+  # Prepare your input for the model
+  instances = [{
+      "prompt": PROMPT.format(prompt=prompt),
+  }]
 
-    logging.error(response)
+  # NB(danenberg): These parameters don't seem to have any effect.
+  parameters = {
+      "max_output_tokens": 256,
+      "temperature": 1.0,
+      "top_p": 1.0,
+      "top_k": 1.0,
+  }
 
-    if not response.predictions:
-        return None
+  request = aiplatform_v1.PredictRequest(
+      endpoint=endpoint, parameters=parameters
+  )
 
-    prediction = response.predictions[0]
+  request.instances.extend(instances)
 
-    logging.error(prediction)
+  # Make the prediction request
+  response = client.predict(request=request)
 
-    # TODO(danenberg): Should be able to do better than this: will truncate anything with `<`.
-    pattern = r"\n<start_of_turn>model\n([^<]*)"
+  logging.error(response)
 
-    match = re.search(pattern, prediction, re.DOTALL)
-
-    logging.error(match)
-
-    if match:
-        return match.group(1)  # Group 1 is the first capturing group (.*?)
-
+  if not response.predictions:
     return None
+
+  prediction = response.predictions[0]
+
+  logging.info(prediction)
+
+  # TODO(danenberg): Should be able to do better than this: will truncate anything with `<`.
+  pattern = r"\n<start_of_turn>model\n([^<]*)"
+
+  match = re.search(pattern, prediction, re.DOTALL)
+
+  logging.error(match)
+
+  if match:
+    return match.group(1)  # Group 1 is the first capturing group (.*?)
+
+  return None
 
 
 @functions_framework.http
 def hello(request):
-    GREETING = "Hi, I'm Gemma! How can I help you today?"
-    HEADERS = {"Content-Type": "text/xml"}
+  GREETING = "Hi, I'm Gemma! How can I help you today?"
+  HEADERS = {"Content-Type": "text/xml"}
 
-    logging.error(request.form.to_dict())
+  logging.error(request.form.to_dict())
 
-    speech_result = request.form.get("SpeechResult")
+  speech_result = request.form.get("SpeechResult")
 
-    suffix = None
+  suffix = None
 
-    if speech_result:
-        suffix = generate(speech_result)
+  if speech_result:
+    suffix = generate(speech_result)
 
-    logging.error(suffix)
+  logging.error(suffix)
 
-    response = VoiceResponse()
-    gather = Gather(
-        input="speech",
-        enhanced="true",
-        speech_model="phone_call",
-        barge_in=True,
-    )
-    gather.append(say(suffix if suffix else GREETING))
-    response.append(gather)
-    response.append(say("Thanks for calling!"))
+  response = VoiceResponse()
+  gather = Gather(
+      input="speech",
+      enhanced="true",
+      speech_model="phone_call",
+      barge_in=True,
+  )
+  gather.append(say(suffix if suffix else GREETING))
+  response.append(gather)
+  response.append(say("Thanks for calling!"))
 
-    return response.to_xml(), HTTPStatus.OK, HEADERS
+  return response.to_xml(), HTTPStatus.OK, HEADERS
